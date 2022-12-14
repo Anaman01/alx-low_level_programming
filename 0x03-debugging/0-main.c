f5a2207128eac0091501bbbede919781b114fc58
Anaman01 <eanaman16@gmail.com>
2022-12-13 23:42:41 -0800

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0; /*Assigning a value to the variable*/
+	positive_or_negative(i); /*Calling the positive or negative function*/
+
+	return (0);
+}
